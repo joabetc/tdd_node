b1e4d84a9c673459543c9e8f21491326c79b6bc9
Joabe Costa <joabetc@hotmail.com>
2018-07-07 16:54:32 -0300

    Creating users file to store data and using fs

--- user-data.js
+++ user-data.js
@@ -1,7 +1,11 @@
+var fs = require('co-fs');
+var userFile = './users.json';
+
 module.exports = {
   users: {
     get: function* () {
-      return [];
+      var data = yield fs.readFile(userFile, 'utf-8');
+      return JSON.parse(data);
     },
     save: function* () {
 
