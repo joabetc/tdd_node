634d261545f5d79435093b18c33a77c3d46000ba
Joabe Costa <joabetc@hotmail.com>
2018-07-07 17:12:42 -0300

    Creating another failing test

--- test/user-api-spec.js
+++ test/user-api-spec.js
@@ -8,12 +8,27 @@ before(function *() {
 });
 
 describe('user data', function() {
-  it('should have +1 user count after saving', function *() {
+  it('should have +1 user count after saving', function* () {
     var users = yield data.users.get();
 
     yield data.users.save({ name: 'John' });
 
     var newUsers = yield data.users.get();
+
+    newUsers.length.should.equal(users.length + 1);
+  });
+});
+
+describe('user web', function() {
+  it('should have +1 user count after saving', function* () {
+    var data = yield request.get('/user').expect(200).end();
+
+    var users = data.body;
+
+    yield data.users.save({ name: 'John' });
+
+    var data = yield request.get('/user').expect(200).end();
+    var newUsers = newData.body;
 
     newUsers.length.should.equal(users.length + 1);
   });
